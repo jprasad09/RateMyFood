dd7e67c4b9eae024b5c940c6f4d2871b5598109a
jprasad09 <phjadhav09@gmail.com>
2023-01-25 20:20:17 +0530

    home page outline

--- client/src/components/Home/Navbar/Navbar.jsx
+++ client/src/components/Home/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import styles from './navbar.module.css'
+import React from "react"
+import styles from "./navbar.module.css"
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className={styles.navContainer}>
+      <h1 className={styles.navTitle}>RateMyFood.</h1>
+      <NavLink to="/login">
+        <button>Login</button>
+      </NavLink>
+    </nav>
+  );
+};
 
 export default Navbar
